5174be1a2165288c5dfd554438f992fdc78b7645
ml4economics <karsten_opitz@arcor.de>
2024-10-13 12:20:00 +0100

    simple shiny

--- bus-dashboard/server.R
+++ bus-dashboard/server.R
@@ -1,26 +1,22 @@
-library(shiny)
-
-# Benutzeroberfläche definieren
-ui <- fluidPage(
-  titlePanel("Beispiel einer Shiny-App"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins", "Anzahl der Balken:", min = 1, max = 50, value = 30)
-    ),
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-)
-
-# Serverlogik definieren
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- faithful[, 2]  # Old Faithful Geysir Daten
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(x, breaks = bins, col = 'darkgray', border = 'white', main = 'Geysir eruptions')
+function(input, output) {
+  
+  output$main_plot <- renderPlot({
+    
+    hist(faithful$eruptions,
+         probability = TRUE,
+         breaks = as.numeric(input$n_breaks),
+         xlab = "Duration (minutes)",
+         main = "Geyser eruption duration")
+    
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+    
+    if (input$density) {
+      dens <- density(faithful$eruptions,
+                      adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+    
   })
 }
-
-# Shiny-App erstellen
-shinyApp(ui = ui, server = server)
--- bus-dashboard/ui.R
+++ bus-dashboard/ui.R
@@ -1,66 +1,25 @@
-library(shinydashboard)
-library(leaflet)
-
-header <- dashboardHeader(
-  title = "Twin Cities Buses"
-)
-
-body <- dashboardBody(
-  fluidRow(
-    column(width = 9,
-      box(width = NULL, solidHeader = TRUE,
-        leafletOutput("busmap", height = 500)
-      ),
-      box(width = NULL,
-        uiOutput("numVehiclesTable")
-      )
-    ),
-    column(width = 3,
-      box(width = NULL, status = "warning",
-        uiOutput("routeSelect"),
-        checkboxGroupInput("directions", "Show",
-          choices = c(
-            Northbound = 4,
-            Southbound = 1,
-            Eastbound = 2,
-            Westbound = 3
-          ),
-          selected = c(1, 2, 3, 4)
-        ),
-        p(
-          class = "text-muted",
-          paste("Note: a route number can have several different trips, each",
-                "with a different path. Only the most commonly-used path will",
-                "be displayed on the map."
-          )
-        ),
-        actionButton("zoomButton", "Zoom to fit buses")
-      ),
-      box(width = NULL, status = "warning",
-        selectInput("interval", "Refresh interval",
-          choices = c(
-            "15 seconds" = 15,
-            "30 seconds" = 30,
-            "1 minute" = 60,
-            "2 minutes" = 120,
-            "5 minutes" = 300,
-            "10 minutes" = 600
-          ),
-          selected = "60"
-        ),
-        uiOutput("timeSinceLastUpdate"),
-        actionButton("refresh", "Refresh now"),
-        p(class = "text-muted",
-          br(),
-          "Source data updates every 15 seconds."
-        )
-      )
-    )
+bootstrapPage(
+  
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+  
+  checkboxInput(inputId = "individual_obs",
+                label = strong("Show individual observations"),
+                value = FALSE),
+  
+  checkboxInput(inputId = "density",
+                label = strong("Show density estimate"),
+                value = FALSE),
+  
+  plotOutput(outputId = "main_plot", height = "300px"),
+  
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+                   sliderInput(inputId = "bw_adjust",
+                               label = "Bandwidth adjustment:",
+                               min = 0.2, max = 2, value = 1, step = 0.2)
   )
-)
-
-dashboardPage(
-  header,
-  dashboardSidebar(disable = TRUE),
-  body
+  
 )
